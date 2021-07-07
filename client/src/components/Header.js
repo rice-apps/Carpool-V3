d054543ff691bbaaaecd07793cb0406ebb5b8bb3
KataTech <kmh9@rice.edu>
2021-07-06 19:20:20 -0500

    Header Implemented

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: Josefin Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 48px;
+    line-height: 48px;
+
+    color: #002140;
+`;
+
+const Subheading = styled.h2`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: Josefin Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 20px;
+
+    color: #BBDAFF;
+`;
 
 const Header = ({title, subtitle}) => {
     return (
         <header className='header'>
-            <h1>{title} </h1>
-            <h2>{subtitle}</h2>
+            <Heading>{title} </Heading>
+            <Subheading>{subtitle}</Subheading>
         </header>
     )
 }
