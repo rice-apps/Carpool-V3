396c96816285caaf05a1a027a39988f6ea5aa4ad
Freda0502 <68989706+Freda0502@users.noreply.github.com>
2021-07-19 10:38:56 -0500

    navbar changge to styled component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,14 +5,69 @@ import { Link } from 'react-router-dom'
 import { SidebarData } from './SidebarData'
 import './Navbar.css'
 import { IconContext } from 'react-icons'
+import styled, { css } from 'styled-components'
 
-// componentDidUpdate (prevProps) {
-//     if (prevProps.location.key !== this.props.location.key) {
-//         this.setState({
-//             isFormSubmitted: false,
-//         })
-//     }
-// }
+//working
+const NavBarDiv = styled.div`
+  background-color: #152238;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: left;
+`
+
+//working
+const MenuBarsLink = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  background: none;
+  to='#'
+`
+//cannot close the navbar
+const NavMenu = styled.nav`
+  left: 0;
+  transition: 350ms;
+  ${(props) => props.sidebar !== 'true'} {
+    background-color: #152238;
+    width: 250px;
+    height: 100vh;
+    display: flex-start;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: -100%;
+    transition: 850ms;
+  }
+`
+// left: ${(sidebar) => (sidebar ? '0' : '-100%')}
+// transition: ${(sidebar) => (sidebar ? '350ms' : '850ms')
+
+// const NavMenu2 = styled.nav`
+//   left: 0;
+//   transition: 350ms;
+//   ${props.sidebar &&
+//   css`
+//     background-color: #152238;
+//     width: 250px;
+//     height: 100vh;
+//     display: flex-start;
+//     justify-content: center;
+//     position: fixed;
+//     top: 0;
+//     left: -100%;
+//     transition: 850ms;
+//   `}
+// `
+
+const NavMenuActive = styled.nav`
+  left: 0;
+  transition: 350ms;
+`
+
+//nav text etc
+const spanDiv = styled.div`
+  margin-left: 16px;
+`
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false)
@@ -20,14 +75,46 @@ function Navbar() {
   const showSidebar = () => setSidebar(!sidebar)
 
   return (
+    // <>
+    //   <IconContext.Provider value={{ color: '#fff' }}>
+    //     <NavBarDiv>
+    //       <MenuBarsLink to='#'>
+    //         <FaIcons.FaBars onClick={showSidebar} />
+    //       </MenuBarsLink>
+    //     </NavBarDiv>
+    //     <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+    //       <NavMenuDiv>
+    //         <ul onClick={showSidebar}>
+    //           <li className='navbar-toggle'>
+    //             <Link to='#' className='menu-bars'>
+    //               <AiIcons.AiOutlineClose />
+    //             </Link>
+    //           </li>
+    //           {SidebarData.map((item, index) => {
+    //             return (
+    //               <li key={index} className={item.cName}>
+    //                 <Link to={item.path}>
+    //                   {item.icon}
+    //                   <spanDiv>{item.title}</spanDiv>
+    //                 </Link>
+    //               </li>
+    //             )
+    //           })}
+    //         </ul>
+    //       </NavMenuDiv>
+    //     </nav>
+    //   </IconContext.Provider>
+    // </>
+
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
-          <Link to='#' className='menu-bars'>
+        <NavBarDiv>
+          <MenuBarsLink>
             <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          </MenuBarsLink>
+        </NavBarDiv>
+        {/* {sidebar ? <NavMenuActive> : <NavMenu>} */}
+        <NavMenu sidebar='true'>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
@@ -50,7 +137,8 @@ function Navbar() {
               )
             })}
           </ul>
-        </nav>
+          {/* {sidebar ? </NavMenuActive> : </NavMenu>} */}
+        </NavMenu>
       </IconContext.Provider>
     </>
   )
