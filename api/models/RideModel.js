eba8eeac7669ee2a152d7311f085a1564eef1247
Freda0502 <68989706+Freda0502@users.noreply.github.com>
2021-07-26 14:12:13 -0500

    added stylings for location and seats left

--- api/models/RideModel.js
+++ api/models/RideModel.js
@@ -1,23 +1,31 @@
-import { composeWithMongoose } from 'graphql-compose-mongoose';
-import { User } from './UserModel';
-import { Location } from './LocationModel';
+import { composeWithMongoose } from 'graphql-compose-mongoose'
+import { User } from './UserModel'
+import { Location } from './LocationModel'
 
-var mongoose = require('mongoose')
-    , Schema = mongoose.Schema
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema
 
 require('../db')
 
 var RideSchema = new Schema({
-    departureDate: { type: Date, default: Date.now() },
-    departureLocation: { type: Schema.Types.ObjectId, ref: Location, required: true },
-    arrivalLocation: { type: Schema.Types.ObjectId, ref: Location, required: true },
-    owner: { type: Schema.Types.ObjectId, ref: User, required: true },
-    riders: [ { type: Schema.Types.ObjectId, ref: User } ],
-    note: String, // notes section for personal message from ride creator       
-    spots: { type: Number, default: 3 },
-    //cost: { type: Number },
-    //ownerDriving: { type: Boolean, default: false },
-});
+  departureDate: { type: Date, default: Date.now() },
+  departureLocation: {
+    type: Schema.Types.ObjectId,
+    ref: Location,
+    required: true,
+  },
+  arrivalLocation: {
+    type: Schema.Types.ObjectId,
+    ref: Location,
+    required: true,
+  },
+  owner: { type: Schema.Types.ObjectId, ref: User, required: true },
+  riders: [{ type: Schema.Types.ObjectId, ref: User }],
+  note: String, // notes section for personal message from ride creator
+  spots: { type: Number, default: 3 },
+  //cost: { type: Number },
+  //ownerDriving: { type: Boolean, default: false },
+})
 
-export const Ride = mongoose.model("rides", RideSchema);
-export const RideTC = composeWithMongoose(Ride);
+export const Ride = mongoose.model('rides', RideSchema)
+export const RideTC = composeWithMongoose(Ride)
--- client/src/Pages/RideSummary.js
+++ client/src/Pages/RideSummary.js
@@ -4,44 +4,110 @@ import { gql, useQuery } from '@apollo/client'
 // import { graphql } from 'react-apollo'
 import { useParams } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
-import { IoLocationSharp } from 'react-icons/io5'
+import { IoLocationSharp, IoShareSocialSharp } from 'react-icons/io5'
+import { IoIosArrowBack } from 'react-icons/io'
 import { AiTwotoneCalendar } from 'react-icons/ai'
 
 //styling
 const SeatsLeftDiv = styled.div`
-  text-align: right;
-  color: #4b6dd1;
+  position: absolute;
+  left: 68.36%;
+  right: 0%;
+  top: 5.03%;
+  bottom: 88.04%;
+  background: rgba(187, 218, 255, 0.22);
+  border-radius: 5px 0px 0px 5px;
+
+  text-align: left;
+  color: #2075d8;
   padding-right: 1vh;
+  font-family: Josefin Sans;
+  font-style: normal;
+  line-height: 11px;
 `
 const SeatsLeftNum = styled.div`
-  margin-top: 4vh;
-  padding-left: 2vh;
-  color: #4b6dd1;
-  font-size: 4vh;
+  position: absolute;
+  left: -64.3%;
+  right: -7.17%;
+  top: 30%;
+  bottom: -65.22%;
+
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 24px;
+  text-align: center;
 `
+const SeatsLeftText = styled.div`
+  position: absolute;
+  left: 35%;
+  right: 25.93%;
+  top: 30%;
+  bottom: 12%;
+
+  font-weight: 300;
+  font-size: 11px;
+  line-height: 11px;
+`
+const SocialIcon = styled.div`
+  position: absolute;
+  left: 70%;
+  right: 0%;
+  top: 30%;
+  bottom: 0%;
+`
+
 const RideSummaryDiv = styled.div`
   margin-top: 4vh;
   padding-left: 2vh;
-  color: #4b6dd1;
-  font-size: 4vh;
+  color: #2075d8;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 18px;
+  text-align: left;
 `
 
 const LocationDiv = styled.div`
-  margin-top: 4vh;
-  text-align: center;
-  padding-left: 2vh;
+  margin-top: 10vh;
+  // text-align: center;
+  // padding-left: 2vh;
   color: #00004d;
-  font-family: Monaco;
-  font-weight: bold;
   font-size: 3 vh;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 35px;
+  line-height: 35px;
+  text-align: center;
 `
 
 const RidersDiv = styled.div`
+  position: absolute;
+  height: 213px;
+  left: 15px;
+  right: 15px;
+  top: 296px;
   margin-top: 4vh;
   text-align: center;
   padding-left: 2vh;
   font-family: Monaco;
   font-size: 1.5 vh;
+  background: rgba(228, 233, 241, 0.31);
+  border-radius: 9px;
+`
+
+const DateDiv = styled.div`
+  margin-top: 4vh;
+  text-align: center;
+  padding-left: 2vh;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 16px;
+  display: flex;
+  color: #002140;
 `
 
 //graphql
@@ -66,9 +132,10 @@ const GET_RIDE = gql`
     }
   }
 `
+
 const RideSummary = () => {
   let { id } = useParams()
-  console.log('id', id)
+  // console.log('id', id)
   const [getVariables, setVariables] = useState({})
   const [rideId, setRideId] = React.useState(null)
 
@@ -85,23 +152,21 @@ const RideSummary = () => {
   if (!data) return <p>No data...</p>
 
   const { rideOne: ride } = data
-  console.log('data', data)
-
-  let {
-    owner,
-    riders,
-    departureDate,
-    departureLocation,
-    arrivalLocation,
-    spots,
-  } = data
+  const time = ride.departureDate
+  // const formattedTime = time.format('MMMM Do YYYY, h:mm:ss a')
+  // console.log(formattedTime)
 
   return (
     <div>
-      <RideSummaryDiv>Ride Summary</RideSummaryDiv>
+      <RideSummaryDiv>
+        <IoIosArrowBack></IoIosArrowBack>&nbsp;Ride Summary
+      </RideSummaryDiv>
       <SeatsLeftDiv>
         <SeatsLeftNum>{ride.spots}</SeatsLeftNum>
-        seats left
+        <SeatsLeftText>seat(s) left</SeatsLeftText>
+        <SocialIcon>
+          <IoShareSocialSharp></IoShareSocialSharp>
+        </SocialIcon>
       </SeatsLeftDiv>
       <LocationDiv>
         <IoLocationSharp></IoLocationSharp>&nbsp;
@@ -109,12 +174,11 @@ const RideSummary = () => {
         <BsArrowRight></BsArrowRight>&nbsp;
         {ride.arrivalLocation.title}
       </LocationDiv>
-      <p>{ride.departureDate}</p>
+      <DateDiv>{ride.departureDate}</DateDiv>
       <AiTwotoneCalendar></AiTwotoneCalendar>
-      <RidersDiv> Riders {ride.riders}</RidersDiv>
+      <RidersDiv> Riders {ride.riders.firstName}</RidersDiv>
     </div>
   )
 }
 
-// export default graphql(getRideQuery)(RideSummary
 export default RideSummary
