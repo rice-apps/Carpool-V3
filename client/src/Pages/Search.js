2c65df2590cf7e663434e64de52fc5d8f578c24f
willie-yao <william2000yao@gmail.com>
2021-10-01 16:04:21 -0500

    Get rid of warnings

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Header from '../components/Header';
 import Form from '../components/Form';
 import DisplayRides from '../components/DisplayRides'
