81910259e86dde78978aef82c21058428b2039d2
Shreyas Minocha <sm-git@riseup.net>
2021-10-02 13:10:43 -0500

    Create alert dialog

---
+++ client/src/components/Alert.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+function AlertDialog() {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Button onClick={handleClickOpen}>
+                Open alert dialog
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    Test test?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Test test. Lorem ipsum doler sit amet.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Nope</Button>
+                    <Button onClick={handleClose} autoFocus>Yep</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
+
+export default AlertDialog;
