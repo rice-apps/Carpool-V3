6c85a9e3c4563cd14e59074ddb23e6f95049de45
Anya Gu <anyagu@Anyas-MacBook-Air.local>
2021-10-02 18:20:43 -0500

    add title and search button to home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,7 +3,8 @@ import logo from '../logo.svg';
 import '../App.css';
 import styled from 'styled-components'
 import { useMediaQuery } from 'react-responsive'
-
+import {SearchButton, SearchButtonDiv} from './HomeStyles.js'
+import { ColorButton } from "../components/Create.styles";
 const Home = () => {
     const MainHeader = styled.header`
         display: flex;
@@ -17,17 +18,21 @@ const Home = () => {
         @media (max-width: 480px) {
             background-color: #553d67;
         }
-    `
 
+    `
+    const toSearch = () => {
+        window.location.replace("/search");
+    }
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 480px)' })
 
     return (
         <div className="App">
+        
         <MainHeader>
             
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-                Edit <code>src/Pages/Hoome/index.js</code> and save to reload.
+                Welcome to Carpool!
             </p>
             <a
                 className="App-link"
@@ -37,7 +42,14 @@ const Home = () => {
             >
             Learn React { isTabletOrMobile && <p>Yo we on mobile</p>}
             </a>
+            <SearchButtonDiv>
+                <SearchButton onClick={() => toSearch()}>
+                    Search for Rides
+                </SearchButton>
+            </SearchButtonDiv>
         </MainHeader>
+        
+        
         </div>
     )
 }
---
+++ client/src/Pages/HomeStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const SearchButtonDiv = styled.div`
+  background: #282c34;
+  margin-top : 20px;
+`
+const SearchButton = styled.button`
+  background: white;
+  color: #61dafb;
+  width: 300px;
+  height: 50px;
+  font-size:30px;
+`
+
+export {
+  SearchButtonDiv,
+  SearchButton
+}
