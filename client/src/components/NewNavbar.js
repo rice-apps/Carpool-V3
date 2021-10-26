ed3e51a3b54010020be561891046ce2d7597b394
Alexis Nicolas <atn4@rice.edu>
2021-10-26 17:53:01 -0500

    changed imports to be more succinct

--- client/src/components/NewNavbar.js
+++ client/src/components/NewNavbar.js
@@ -1,19 +1,12 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/MenuRounded';
 import HomeIcon from '@material-ui/icons/HomeRounded';
 import CarIcon from '@material-ui/icons/DirectionsCarRounded';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import { Drawer } from '@material-ui/core';
+import { List, ListItemAvatar, ListItem, ListItemIcon, ListItemText, 
+  Drawer, Divider, IconButton, AppBar, Toolbar } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
