dee545a487acb14063b14498ed24693ef4936339
Alexis Nicolas <atn4@rice.edu>
2021-10-27 16:23:45 -0500

    shows the right component whether a user is logged in!
    added a username function that shows the user's name and avatar, and also login function that has a button that routes to the login page

--- client/src/components/NewNavbar.js
+++ client/src/components/NewNavbar.js
@@ -1,11 +1,9 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/MenuRounded';
 import HomeIcon from '@material-ui/icons/HomeRounded';
 import CarIcon from '@material-ui/icons/DirectionsCarRounded';
-import { List, ListItemAvatar, ListItem, ListItemIcon, ListItemText, 
+import { List, ListItem, ListItemIcon, ListItemText, 
   Drawer, Divider, IconButton, AppBar, Toolbar, Avatar } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router';
@@ -62,29 +60,42 @@ export default function ButtonAppBar() {
   if (error) return `Error! ${error.message}`;
 
   const {userOne: user} = userData;
-  if (!user) return <div>Invalid User ID</div>
+
+  const username = () => (
+    <ListItem button component = {Link} to = "/profile" style = {{gap: "5vw"}}>
+      <Avatar className = {classes.large}/>
+      <ListItemText className = {classes.text} primary = {user.firstName + ' ' + user.lastName}/>
+    </ListItem>
+  )
+
+  const login = () => (
+    <div>
+      <ListItem button component = {Link} to = "/login" style = {{}}>
+        <ListItemText className = {classes.text}  primary = "Login"/>
+      </ListItem>
+      <Divider/>
+    </div>
+  )
+
 
   // Eventually should make this extensible
   const drawerItems = () => (
-      <div>
-        <List className = {classes.list} style = {{width: "66vw", height: "100vh"}}>
-          <ListItem button component = {Link} to = "/profile" style = {{gap: "5vw"}}>
-            <Avatar className = {classes.large}/>
-            <ListItemText className = {classes.text} primary = {user.firstName + ' ' + user.lastName}/>
-          </ListItem>
-          <ListItem button component = {Link} to = "/home">
-            <ListItemIcon className= {classes.icon}> <HomeIcon/> </ListItemIcon>
-            <ListItemText className = {classes.text} primary = "Home"/>
-          </ListItem>
-          <ListItem button component = {Link} to = "/search">
-            <ListItemIcon className = {classes.icon}> <CarIcon/> </ListItemIcon>
-            <ListItemText className = {classes.text} primary = "Your Rides"/>
-          </ListItem>
-          <ListItem button className = {classes.bottom} component = {Link} to = "/about">
-            <ListItemText className = {classes.text} primary = "About"/>
-          </ListItem>
-        </List>
-      </div>
+    <div>
+      <List className = {classes.list} style = {{width: "66vw", height: "100vh"}}>
+        {user ? username() : login()}
+        <ListItem button component = {Link} to = "/home">
+          <ListItemIcon className= {classes.icon}> <HomeIcon/> </ListItemIcon>
+          <ListItemText className = {classes.text} primary = "Home"/>
+        </ListItem>
+        <ListItem button disabled = {!user} component = {Link} to = "/search">
+          <ListItemIcon className = {classes.icon}> <CarIcon/> </ListItemIcon>
+          <ListItemText className = {classes.text} primary = "Your Rides"/>
+        </ListItem>
+        <ListItem button className = {classes.bottom} component = {Link} to = "/about">
+          <ListItemText className = {classes.text} primary = "About"/>
+        </ListItem>
+      </List>
+    </div>
   );
 
   return (
