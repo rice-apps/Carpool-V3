c01a112a9e6aaf9b1cf9a43de75bc3a92bbd3f70
willie-yao <william2000yao@gmail.com>
2021-10-30 13:53:49 -0500

    Moved props around

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header';
 import Form from '../components/Form';
 import DisplayRides from '../components/DisplayRides'
+import { gql, useQuery } from "@apollo/client";
 import "@fontsource/source-sans-pro";
 import './Search.css'
 
@@ -17,11 +18,56 @@ const Search = () => {
     resultDestArr = rides;
   }
 
+  const GET_RIDES = gql`
+  query {
+    rideMany {
+      _id
+      departureDate
+      riders {
+        netid
+        firstName
+        lastName
+      }
+      spots
+      departureLocation {
+        title
+        address
+      }
+      arrivalLocation {
+        title
+        address
+      }
+      owner {
+        netid
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+  const GET_LOCATIONS = gql`
+    query {
+      locationMany {
+        title
+        address
+      }
+    }
+  `;
+
+  const { refetch: refetchLoc } = useQuery(GET_LOCATIONS);
+
+  const { refetch: refetchRide, loading: rideLoading } = useQuery(GET_RIDES,
+    {
+      variables: {}
+    }
+  );
+
   return (
     <React.Fragment>
       <div><Header subtitle = "Search Rides"/></div>
-      <Form resultRides={resultDestArr} setResultRides={(rides) => {updateResultRides(rides)}} displayRef={displayRef} />
-      <DisplayRides ref={displayRef} rides={resultDestArr} testVar={3}/>
+      <Form resultRides={resultDestArr} setResultRides={(rides) => {updateResultRides(rides)}} displayRef={displayRef} getRidesRefetch={() => refetchRide()} getLocsRefetch={() => refetchLoc()} />
+      <DisplayRides ref={displayRef} rides={resultDestArr} rideLoading={rideLoading} testVar={3}/>
     </React.Fragment>
   )
 }
