ecfd4c19cc759b7e8a79386dea0124062874ffc4
ajk8703 <ajkim8703@gmail.com>
2021-10-30 14:33:49 -0500

    Set up Past Ride Card
    I wanted to c/p upcoming into past before I make the coloured in section break off and be a little bubble because we need it as it is for pasts rides card (pill style coloured)

--- client/src/Pages/YourRidesStyles.js
+++ client/src/Pages/YourRidesStyles.js
@@ -5,6 +5,8 @@ const OverallPage = styled.div`
 display: flex;
 align-items: center;
 flex-direction: column;
+font-family: Josefin Sans;
+
 `;
 
 const Title = styled.div`
@@ -13,14 +15,20 @@ justify-content: center;
 align-items: center;
 flex-direction: column;
 width: 80vw;
+height: 10vh;
+font-family: Josefin Sans;
+font-size: 6vh;
+font-style: normal;
+font-weight: 100;
+text-align: center;
 
 `;
 
 const UpcomingRidesSection = styled.div`
 dislay: flex;
 flex-direction: column;
-width: 80vw;
-
+width: 90vw;
+padding: 3%;
 background: #F4F6F9;        /* light mode background */
 border-radius: 5px;
 
@@ -30,15 +38,23 @@ const PastRidesandPayments = styled.div`
 display: flex;
 width: 80vw;
 justify-content: space-between
+font-family: Josefin Sans;
+
 `;
 
 const PastRideCard = styled.div`
+font-family: Josefin Sans;
+
 `;
 
 const Unpaid = styled.div`
+font-family: Josefin Sans;
+
 `;
 
 const Paid = styled.div`
+font-family: Josefin Sans;
+
 `;
 
 
