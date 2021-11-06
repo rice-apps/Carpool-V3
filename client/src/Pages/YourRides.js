dc49c8e6887b30c5240e79ac0db31afaa6f323f7
William Yao <47200969+willie-yao@users.noreply.github.com>
2021-11-06 13:38:26 -0500

    Your rides queries (#41)
    * Added query
    
    * Got one ride

--- client/src/Pages/YourRides.js
+++ client/src/Pages/YourRides.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import UpcomingRideCard from '../components/UpcomingRideCard.js';
 import PastRideCard from '../components/PastRideCard.js';
+import { gql, useQuery} from "@apollo/client";
+import moment from "moment";
+
 
 import { 
     OverallPageTitle, 
@@ -13,7 +16,71 @@ import {
     } from './YourRidesStyles';
 
 
+const GET_RIDE = gql`
+query GetRide($netID: String) {
+	userOne (filter:{netid: $netID}) { 
+			rides {
+				_id
+				departureDate
+				riders {
+						netid
+						firstName
+						lastName
+				}
+				spots
+				departureLocation {
+						title
+						address
+				}
+				arrivalLocation {
+						title
+						address
+				}
+				owner {
+						netid
+						firstName
+						lastName
+				}
+			}     
+	}
+} 
+`
+
+
 const YourRides = (paid) => {
+
+    let netid = localStorage.getItem("netid")
+
+		// const [allRides, setAllRides] = useEffect([])
+		const [prevRides, setPrevRides] = useState([])
+		const [futureRides, setFutureRides] = useState([])
+    const { data, loading, error } = useQuery(GET_RIDE, {
+        variables: { netid: netid },
+      });
+    ;
+
+    useEffect(() => {
+        if (data) {
+						let rides = data["userOne"]["rides"]
+
+						console.log("rides", rides)
+
+						let previousrides = rides.filter(ride => moment(ride.departureDate) < new Date())
+						previousrides.sort((a, b) => moment(b.departureDate) - moment(a.departureDate))
+						let upcomingrides = rides.filter(ride => moment(ride.departureDate) >= new Date())
+						upcomingrides.sort((a, b) => moment(b.departureDate) - moment(a.departureDate))
+
+						console.log("previousRides", previousrides)
+						console.log("upcomingRides", upcomingrides)
+
+						setPrevRides(previousrides)
+						setFutureRides(upcomingrides)
+        }
+    }, [data])
+
+    if (error) return <p>Error...</p>;
+    if (loading) return <p>loading...</p>;
+
     return (
         <div> 
             <OverallPage>
