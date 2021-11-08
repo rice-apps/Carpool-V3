1b4758f878f226a0accad5aa2d3c714da8850a89
anyaguuu <91390685+anyaguuu@users.noreply.github.com>
2021-11-07 20:15:31 -0600

    Landing page CSS (#43)
    * nochange
    
    * make popup
    
    * Make popup
    
    * add edit icon to popup
    
    * make dialog bigger
    
    * add file
    
    * landing page css
    
    * minor change
    
    * landing page css
    
    Co-authored-by: Anya Gu <anyagu@Anyas-MacBook-Air.local>
    Co-authored-by: Anya Gu <anyagu@anyas-air.dyndns.rice.edu>

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,39 +1,23 @@
 import React from "react";
 import '../App.css';
-import logo from '../logo.svg';
-import { useMediaQuery } from 'react-responsive'
 import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button'
-import {MainHeader} from './HomeStyles.js'
+import {WelcomeHeader, CarpoolHeader, Header, SearchButton, ButtonContainer} from './HomeStyles.js'
 
 const Home = () => {
 
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 480px)' })
-
     return (
         <div className="App">
-        
-        <MainHeader>
-            
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Welcome to Carpool!
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React { isTabletOrMobile && <p>Yo we on mobile</p>}
-            </a>
-            <Link to= "/search">
-                <Button variant="contained">Search for Rides</Button>
-                
-            </Link>
-        </MainHeader>
-        
-        
+        <Header>
+            <WelcomeHeader>
+                Welcome to
+            </WelcomeHeader>
+            <CarpoolHeader>
+                Rice Carpool
+            </CarpoolHeader>
+        </Header>
+        <ButtonContainer>
+                <Link to="/search"><SearchButton>Start Riding</SearchButton></Link>
+        </ButtonContainer>
         </div>
     )
 }
