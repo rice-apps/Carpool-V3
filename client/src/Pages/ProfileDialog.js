1edcc5afcafb68e127bf4642dfbdb9220bacb6a2
anyaguuu <91390685+anyaguuu@users.noreply.github.com>
2021-11-13 00:44:20 -0600

    Profiledialog css (#47)
    * nochange
    
    * make popup
    
    * Make popup
    
    * add edit icon to popup
    
    * make dialog bigger
    
    * add file
    
    * landing page css
    
    * minor change
    
    * landing page css
    
    * started css for profile form
    
    * not finished yet profile dialog css
    
    * profile dialog css half complete
    
    * Fixed warnings
    
    Co-authored-by: Anya Gu <anyagu@Anyas-MacBook-Air.local>
    Co-authored-by: Anya Gu <anyagu@anyas-air.dyndns.rice.edu>
    Co-authored-by: William Yao <william2000yao@gmail.com>

--- client/src/Pages/ProfileDialog.js
+++ client/src/Pages/ProfileDialog.js
@@ -1,6 +1,9 @@
-import {Header, EditName, EditContactInfo, EditPaymentOptions, SubmitButton} from './ProfileDialogStyles.js';
+import {Popup, NameSectionHeader, ContactSectionHeader,
+    PaymentSectionHeader, PhoneTextBox, EmailTextBox, 
+    SubmitButton, FirstNameTextBox, LastNameTextBox, PaymentDropdown, 
+    IconContainer, PaymentTextBox} from './ProfileDialogStyles.js';
 import React, {useState} from 'react';
-import {Dialog, DialogContent, TextField} from '@material-ui/core';
+import {Dialog, InputAdornment, TextField} from '@material-ui/core';
 import {DialogActions} from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
@@ -8,8 +11,41 @@ import Box from '@material-ui/core/Box';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import {makeStyles} from '@material-ui/core/styles'
+import CloseIcon from '@material-ui/icons/Close';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+
+const useStyles = makeStyles((theme) =>({
+    input :{
+        widdth: "55vw",
+    },
+    inputLabel: {
+        fontSize: 15,
+        color: "#2075D8",
+    },
+    inputContent: {
+        height: 30,
+        marginTop: 6,
+        background: "rgb(187,218,255,0.22)"
+    },
+    center: {
+        display: "flex",
+        justifycontent: "center",
+        alignItems: "center",
+    },
+    closeButton: {
+        positin: "absolute",
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500]
+    },
+    multiLineColor:{
+        color: "#2075D8"
+    }
+}));
 
 export default function ProfileDialog (props) {
+    const classes = useStyles();
 
     const[payment, setPaymentOption] = useState('');
     
@@ -25,27 +61,95 @@ export default function ProfileDialog (props) {
 
     return (
         <div>
-            <Dialog open={openDialog} onClose={closeDialog} fullWidth={true}>
-    
-            <DialogContent>
-                <Header>
-                    <p>
-                        Edit Profile
-                    </p>
-                </Header>
-                <EditName>
-                    Name
-                    <TextField id = "outlined-filled" label = "First Name" variant = "filled"/>
-                    <TextField id = "outlined-filled" label = "Last Name" variant = "filled"/>
-                </EditName>
-                <EditContactInfo>
-                    Contact Information
-                    <TextField id = "outlined-filled" label = "Email" variant = "filled"/>
-                    <TextField id = "outlined-filled" label = "Phone #" variant = "filled"/>
-                </EditContactInfo>
-                <EditPaymentOptions>
-                    Payment
-                    <Box sx = {{minWidth: 120}}>
+            <Dialog open={openDialog}
+            onClose={closeDialog} 
+            className={[classes.dialog, classes.center]}
+            fullWidth={true} 
+            maxWidth="lg"
+            id="dialog"
+            >
+            <IconContainer>
+            <AccountCircleIcon style={{fontSize: 130}}/>
+            </IconContainer>
+            <Popup>
+                <NameSectionHeader> Name </NameSectionHeader>
+                <FirstNameTextBox>
+                    <TextField 
+                    id = "filled-basic" 
+                    label = "First Name"
+                    variant = "filled"
+                    fullWidth={true}
+                    className = {classes.input}
+                    InputProps={{
+                        className: classes.inputContent,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <CloseIcon/>
+                            </InputAdornment>
+                        ),
+                        }}
+                    InputLabelProps={{className:[classes.multiLineColor, classes.inputLabel]}}
+                    />
+                </FirstNameTextBox>
+                <LastNameTextBox>
+                <TextField 
+                    id = "filled-basic" 
+                    label = "Last Name"
+                    variant = "filled"
+                    fullWidth={true}
+                    className = {classes.input}
+                    InputProps={{
+                        className: classes.inputContent,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <CloseIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    InputLabelProps={{className:[classes.multiLineColor, classes.inputLabel]}}
+                    />
+                </LastNameTextBox>
+                <ContactSectionHeader>Contact</ContactSectionHeader>
+                <PhoneTextBox> 
+                <TextField 
+                    id = "filled-basic" 
+                    label = "Phone #"
+                    variant = "filled"
+                    fullWidth={true}
+                    className = {classes.input}
+                    InputProps={{
+                        className: classes.inputContent,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <CloseIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    InputLabelProps={{className:[classes.multiLineColor, classes.inputLabel]}}
+                    />
+                </PhoneTextBox>
+                <PaymentSectionHeader>Payment</PaymentSectionHeader>
+
+                <EmailTextBox>
+                <TextField 
+                    id = "filled-basic" 
+                    label = "Email"
+                    variant = "filled"
+                    fullWidth={true}
+                    className = {classes.input}
+                    InputProps={{
+                        className: classes.inputContent,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <CloseIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    InputLabelProps={{className:[classes.multiLineColor, classes.inputLabel]}}
+                    />
+                </EmailTextBox>
+                <PaymentDropdown>
+                    <Box sx = {{minWidth: 210, maxHeight: 20}}>
                         <FormControl fullWidth>
                             <InputLabel id="demo-simple-select-label">Options</InputLabel>
                             <Select
@@ -61,14 +165,30 @@ export default function ProfileDialog (props) {
                             </Select>
                         </FormControl>
                     </Box>
-                    <br></br>
-                    <TextField id = "outlined-filled" label = "Pay to @" variant = "filled"/>
-                </EditPaymentOptions>
+                </PaymentDropdown>
+                <PaymentTextBox>
+                <TextField 
+                    id = "filled-basic" 
+                    label = "@ VenmoID"
+                    variant = "filled"
+                    fullWidth={true}
+                    className = {classes.input}
+                    InputProps={{
+                        className: classes.inputContent,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <CloseIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    InputLabelProps={{className:[classes.multiLineColor, classes.inputLabel]}}
+                    />
+                </PaymentTextBox>
             <SubmitButton>
-                <Button variant="contained">Submit</Button>
+                <Button variant="contained">Save</Button>
             </SubmitButton>
-            </DialogContent>
             <DialogActions></DialogActions>
+            </Popup>
             </Dialog>
         </div>
     )
--- client/src/Pages/ProfileDialogStyles.js
+++ client/src/Pages/ProfileDialogStyles.js
@@ -1,44 +1,107 @@
 import styled from 'styled-components'
 
-
-const Header = styled.header`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: 1.2em;
-    min-height: 10vh;
-    background-color: white;
-    color: navy;
+const Popup = styled.div`
+   width: 77vw;
+   height: 77vh;
+  border-radius: 25px;
+  color: white;
+  left: 20vw;
+  position:0vw;
 `
-const EditName = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 1vh;
-    background-color: white;
-    color: navy;
+const IconContainer = styled.div`
+    position: relative;
+    top: 3.5vh;
+    left: 29.45vw;
+`
+const NameSectionHeader = styled.header`
+    font-family: Josefin Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 3px;
+    color: #2075D8;
+    position: relative;
+    top: 9vh;
+    left: 10vw;
+`
+const FirstNameTextBox = styled.div`
+    position: relative;
+    width: 58vw;
+    height: 5vh;
+    top: 10vh;
+    left: 9vw;
+    border-radius:2.0em;    
+`
+const LastNameTextBox = styled.div`
+    position: relative;
+    width: 58vw;
+    height: 5vh;
+    top: 11vh;
+    left: 9vw;
+    border-radius:2.0em;
 `
 
-const EditContactInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 3vh;
-    background-color: white;
-    color: navy;
+const ContactSectionHeader = styled.header`
+    font-family: Josefin Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 3px;
+    color: #2075D8;
+    position: relative;
+    top: 16vh;
+    left: 10vw;
 `
 
-const EditPaymentOptions = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 3vh;
-    background-color: white;
+const PhoneTextBox = styled.div`
+    position: relative;
+    width: 58vw;
+    height: 5vh;
+    top: 17vh;
+    left: 9vw;
+    border-radius:2.0em;
+`
+const EmailTextBox = styled.div`
+    position: relative;
+    width: 58vw;
+    height: 5vh;
+    top: 17vh;
+    left: 9vw;
+    border-radius:2.0em;
+`
+
+const PaymentSectionHeader = styled.header`
+    font-family: Josefin Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 3px;
+    color: #2075D8;
+    position: relative;
+    top: 26vh;
+    left: 10vw;
+`
+
+const PaymentDropdown = styled.div`
+    display: flex;    
+    flex-direction: column;    
+    align-items: center;    
+    justify-content: center;    
+    top: 60vh;
+    left: 9.5vw;
+
+    background-color: white;    
     color: navy;
+    position: absolute;
+`
+
+const PaymentTextBox = styled.div`
+    position: relative;
+    width: 58vw;
+    height: 5vh;
+    top: 30vh;
+    left: 9vw;
+    border-radius:2.0em;
 `
 
 const SubmitButton = styled.div`
@@ -46,19 +109,28 @@ const SubmitButton = styled.div`
     flex-direction: column;
     aligh-items: center;
     justify-content: center;
-    padding-top: 6vh;
     height:10vh;
-    width:20vw;
+    width: 70vw;
     margin: auto;
+    top: 29vh;
+    left: 0.3vw;
+    position: relative;
 `
 const HideAppBar = styled.div`
 `
 
 export {
-    Header,
-    EditName,
-    EditContactInfo,
-    EditPaymentOptions,
     SubmitButton,
+    NameSectionHeader,
+    ContactSectionHeader,
+    PaymentSectionHeader,
+    FirstNameTextBox,
+    LastNameTextBox,
+    PhoneTextBox,
+    EmailTextBox,
+    PaymentTextBox,
+    Popup,
+    PaymentDropdown,
+    IconContainer,
     HideAppBar
 }
