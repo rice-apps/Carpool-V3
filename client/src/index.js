7838053fa8a88217ad0f8f313c09d1b679918d3d
willie-yao <william2000yao@gmail.com>
2021-11-20 13:00:02 -0600

    Added basename

--- client/src/index.js
+++ client/src/index.js
@@ -31,7 +31,7 @@ export const history = createBrowserHistory();
 render(
     <ThemeProvider theme = {theme}>
         <ApolloProvider client={client}>
-        <Router history={history}>
+        <Router history={history} basename={process.env.PUBLIC_URL}>
             <ToastProvider>
                 <Routes />
             </ToastProvider>
