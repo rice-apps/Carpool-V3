f4b268c1cae6a5cb7274f2c701b7bbc173f5a394
willie-yao <william2000yao@gmail.com>
2021-11-20 14:44:53 -0600

    Updated cors

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ app.use(bodyParser.urlencoded({
 // Apply cors for dev purposes
 app.use(cors({
     // Set CORS options here
-    // origin: "*"
+    origin: "*"
 }))
 
 // Add JWT so that it is AVAILABLE; does NOT protect all routes (nor do we want it to)
