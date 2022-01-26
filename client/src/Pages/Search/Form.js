2c72d3cb462f76a6c1b11fbe7414de8199e4d2c6
henryyqin <50307418+henryyqin@users.noreply.github.com>
2022-01-26 12:49:28 -0600

    removed WRC from list of locations (#88)
    Co-authored-by: Henry Qin <hq5@rice.edu>

--- client/src/Pages/Search/Form.js
+++ client/src/Pages/Search/Form.js
@@ -26,11 +26,6 @@ let PossibleLocations = [
     _id: "60dd1fff8211a44ac40b33f2",
     title: "IAH",
     address: "2800 N Terminal Rd, Houston, TX 77032, USA"
-  },
-  {
-    _id: "60dd20008211a44ac40b33f3",
-    title: "WRC",
-    address: "6330 Main St, Houston, TX 77005, USA"
   }];
 
   // May cause 401 error if a request is made to the database before it's ready
