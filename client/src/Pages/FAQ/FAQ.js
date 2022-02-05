27d723727bde7705bec0a14f9b09d08ac8d79d17
ajk8703 <ajkim8703@gmail.com>
2022-02-05 14:55:54 -0600

    Create FAQ.js

---
+++ client/src/Pages/FAQ/FAQ.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import UpcomingRideCard from './UpcomingRideCard.js';
+import PastRideCard from './PastRideCard.js';
+import { gql, useQuery} from "@apollo/client";
+import moment from "moment";
+
+const FAQ = (paid) => {
+
+    return(
+
+        
+    )
+
+}
