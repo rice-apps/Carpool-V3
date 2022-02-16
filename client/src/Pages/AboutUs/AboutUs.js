356fd638c632d57f10bd4a652a46d95f08cbade2
ajk8703 <ajkim8703@gmail.com>
2022-02-16 14:49:59 -0600

    Create AboutUs files

---
+++ client/src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUs = () => {
+
+    return (
+        <AllDiv>
+            <AboutUsHeader>Meet the Team</AboutUsHeader>
+                <Member>
+
+                </Member>
+
+            
+        </AllDiv>
+
+
+    )
+
+}
+
+export default AboutUs;
---
+++ client/src/Pages/AboutUs/AboutUsstyles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+
+const AllDiv = styled.div`
+  background-color: #012E62;
+  padding-top: 6vh;
+  padding: 2vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-family: Josefin Sans;
+`
+
+const AboutUsHeader = styled.div`
+  align-text: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 3.4em;
+  display: flex;
+  color: white;
+`
+
+const Member = styled.div`
+
+`
+
+
+export {
+  AllDiv,
+  AboutUsHeader,
+  Member
+} 
