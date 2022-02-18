8ef041ccc989db553ca659a072a0dbbb195a4c7c
Kai Hung <kmh9@rice.edu>
2022-02-18 01:25:12 -0600

    Onboarding form styling (#111)
    * Updated the CSS
    
    * Deleted profile icon
    
    * Replaced ProfileForm with OnboardingForm

---
+++ client/src/Pages/Onboarding/OnboardingForm.js
@@ -0,0 +1,89 @@
+import {
+  Header,
+  ProfileFormContainer,
+  Label, 
+  InputBox, 
+  SubmitButton,
+  CancelButton,
+  InputTextField,
+  RequiredTextField
+} from "./OnboardingFormStyle.js";
+import React, { useState } from "react";
+
+const OnboardingForm = ({ onSubmit, onCancel }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [venmo, setVenmo] = useState(undefined);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        return onSubmit({
+            firstName, lastName,
+            phone, venmo
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ProfileFormContainer>
+                <Header> Sign up to use Carpool</Header>
+                <InputBox>
+                    <Label>Name</Label>
+                    <RequiredTextField
+                        label="First Name"
+                        defaultValue={firstName}
+                        name="firstName"
+                        required onChange={(e) => setFirstName(e.target.value)}
+                    ></RequiredTextField>
+                    <RequiredTextField
+                        label="Last Name"
+                        name="lastName"
+                        defaultValue={lastName}
+                        required onChange={(e) => setLastName(e.target.value)}
+                    ></RequiredTextField>
+                </InputBox>
+                <InputBox>
+                    <Label>Contact</Label>
+                    <RequiredTextField
+                        label="Phone #"
+                        defaultValue={phone}
+                        name="phone"
+                        required onChange={(e) => setPhone(e.target.value)}
+                    ></RequiredTextField>
+                </InputBox>
+                <InputBox>
+                    <Label>Venmo</Label>
+                    <InputTextField
+                    label="Account ID"
+                    name="venmo"
+                    defaultValue={venmo}
+                    value={venmo}
+                    onChange={(e) => setVenmo(e.target.value)}
+                    ></InputTextField>
+                </InputBox>
+                <SubmitButton 
+                    variant="contained"
+                    onClick={() => {
+                        onSubmit({
+                        firstName,
+                        lastName,
+                        phone,
+                        venmo,
+                        });
+                    }}>
+                        Submit
+                </SubmitButton>
+                <CancelButton
+                    variant="contained"
+                    onClick={() => { onCancel()}
+                    }>
+                        Cancel
+                </CancelButton>
+            </ProfileFormContainer>
+        </form>
+    ) 
+}
+
+export default OnboardingForm;
---
+++ client/src/Pages/Onboarding/OnboardingFormStyle.js
@@ -0,0 +1,147 @@
+import styled from "styled-components";
+import React from "react";
+import Box from "@material-ui/core/Box";
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  InputLabel,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
+import { InputAdornment } from "@material-ui/core";
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 2.5vh;
+  line-height: 2vh;
+  color: #2075d8; 
+`;
+
+export const Label = styled(InputLabel)({
+  fontFamily: "Josefin Sans",
+  color: "#2075d8",
+  paddingBottom: "1vh"
+});
+
+
+export const InputBox = styled(Box)({
+  height: "16%",
+  width: "76%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-around",
+});
+
+export const ProfileFormContainer = styled.div`
+  display: flex;
+  height: 90vh;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const ProfileStyles = makeStyles(() => ({
+  inputContent: {
+    background: "rgb(187,218,255,0.22)",
+    color: "#2075D8",
+    height: "5vh",
+  },
+  inputLabel: {
+    fontSize: 15,
+    color: "#2075D8",
+  }
+}));
+
+
+export function InputTextField(props) {
+  const classes = ProfileStyles();
+  const { label, name, defaultValue, onChange, value} = props;
+  return (
+    <TextField
+      name={name}
+      style={{paddingBottom: "1vh"}}
+      variant="filled"
+      label={label}
+      defaultValue={defaultValue}
+      fullWidth={true}
+      onChange={onChange}
+      value={value}
+      InputLabelProps={{
+        className: [classes.inputLabel],
+      }}
+      InputProps={{
+        className: classes.inputContent,
+        style: { background: "rgb(187,218,255,0.22)", color: "#2075D8", height: "6vh" },
+        endAdornment: (
+          <InputAdornment position="end">
+          </InputAdornment>
+        ),
+      }}
+    ></TextField>
+  );
+}
+
+export function RequiredTextField(props) {
+  const classes = ProfileStyles();
+  const { label, name, defaultValue, onChange, value} = props;
+  return (
+    <TextField
+      required
+      name={name}
+      style={{paddingBottom: "1vh"}}
+      variant="filled"
+      label={label}
+      defaultValue={defaultValue}
+      fullWidth={true}
+      onChange={onChange}
+      value={value}
+      InputLabelProps={{
+        className: [classes.inputLabel],
+      }}
+      InputProps={{
+        className: classes.inputContent,
+        style: { background: "rgb(187,218,255,0.22)", color: "#2075D8", height: "6vh" },
+        endAdornment: (
+          <InputAdornment position="end">
+          </InputAdornment>
+        ),
+      }}
+    ></TextField>
+  );
+}
+
+export const SubmitButton = withStyles({
+  root: {
+      background: '#2075D8',
+      width: '75vw',
+      borderRadius: 8,
+      border: 0,
+      color: 'white',
+      height: 48,
+  },
+  label: {
+    textTransform: 'capitalize',
+    fontFamily: 'Josefin Sans'
+  },
+})(Button);
+
+export const CancelButton = withStyles({
+  root: {
+      background: '#EB5248',
+      width: '75vw',
+      borderRadius: 8,
+      border: 0,
+      color: 'white',
+      height: 48,
+  },
+  label: {
+    textTransform: 'capitalize',
+    fontFamily: 'Josefin Sans'
+  },
+})(Button);
