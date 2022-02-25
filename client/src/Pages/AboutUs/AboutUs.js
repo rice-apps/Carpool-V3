d25735d4b4daff0397af9e73b53ae92e994c8618
ajk8703 <ajkim8703@gmail.com>
2022-02-25 13:37:10 -0600

    Merge FAQ and About Us into one page

--- client/src/Pages/AboutUs/AboutUs.js
+++ client/src/Pages/AboutUs/AboutUs.js
@@ -1,6 +1,13 @@
 import React from "react";
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import{
     AllDiv,
+    FAQHeader,
+    Questions,
+    QuestionAccordion,
     AboutUsHeader,
     Member,
     Picture,
@@ -14,6 +21,151 @@ const AboutUs = () => {
 
     return (
         <AllDiv>
+
+<FAQHeader>
+                FAQ
+            </FAQHeader>
+
+            <Questions>
+                    
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>Who can use Carpool?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                    Carpool was created for the Rice University Community. A Net ID is required.
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>Do I need to download Carpool on my phone?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                    No, Carpool is not a mobile app. It was created as a mobile-friendly website to be accessible to all devices, with desktop support coming soon. You can add a bookmark/shortcut to the Carpool site to your phone homepage for quicker access!
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>How do I sign up for Carpool?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                    All you need is your Rice Net ID to log onto the site: carpool.riceapps.org
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>I've signed in for the first time, now what?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                    Fill out your profile with your name, phone number, email, and payment information. Now you can view, join, and create rides!
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>How do I see my past and upcoming rides?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>You can find the rides you’ve taken in the past and the rides you’ve scheduled listed on your profile.
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>Where can I travel?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>Currently, the location options are limited to airports and Rice only. Let us know what other locations you’d like to carpool to via this: https://forms.gle/WFqf77FxSy8FVHgb9 
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>If I leave a ride, will the ride disappear?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>If you created a ride and were the only one on the ride, the ride will be deleted. If you joined a ride with other existing riders, the ride will exist but you will no longer be listed as a rider.
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>How can I delete a ride?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>You can only leave a ride. If you are the last rider to leave, the ride will automatically be deleted.
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+                <QuestionAccordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                    <Typography>Is my information shared with outside parties?</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>No, your information is not shared outside the application. It is only used for coordinating rides with your fellow Rice Owls.
+                    </Typography>
+                    </AccordionDetails>
+                </QuestionAccordion>
+
+            </Questions>
+
             <AboutUsHeader>Meet the Team</AboutUsHeader>
                 <Member>
                     
--- client/src/Pages/AboutUs/AboutUsstyles.js
+++ client/src/Pages/AboutUs/AboutUsstyles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
-
-
+import Accordion from '@material-ui/core/Accordion';
 
 const AllDiv = styled.div`
   background-color: #012E62;
@@ -12,6 +11,26 @@ const AllDiv = styled.div`
   align-items: center;
   flex-direction: column;
   font-family: Josefin Sans;
+`
+
+const FAQHeader = styled.div`
+  align-text: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 3.4em;
+  display: flex;
+  color: white;
+`
+
+const Questions = styled.div`
+  padding-top: 5%;
+`
+
+const QuestionAccordion = styled(Accordion)`
+  border-radius: 10px;
+  margin: 4%;
+  filter: drop-shadow(2px 2px 2px #002140);
 `
 
 const AboutUsHeader = styled.div`
@@ -73,6 +92,9 @@ const Title = styled.div`
 
 export {
   AllDiv,
+  FAQHeader,
+  Questions,
+  QuestionAccordion,
   AboutUsHeader,
   Member,
   Picture,
