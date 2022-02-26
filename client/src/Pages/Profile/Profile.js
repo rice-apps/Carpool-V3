d0e26f40d5e21f22419663d47d75fbab11a5b535
Anya Gu <anyagu@Anyas-MacBook-Air.local>
2022-02-26 13:30:00 -0600

    make everyone's profile pic the default account circle icon

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -5,7 +5,7 @@ import { useToasts } from "react-toast-notifications";
 import ProfileDialog from "./ProfileDialog.js";
 import IconButton from "@material-ui/core/IconButton";
 import EditIcon from "@material-ui/icons/Edit";
-import PhoneIcon from '@material-ui/icons/PhoneInTalkOutlined';
+import PhoneIcon from "@material-ui/icons/PhoneInTalkOutlined";
 
 import {
   ButtonBox,
@@ -20,8 +20,8 @@ import {
   StyledText3,
   EditProfileButton,
   College,
-  UserPic,
-  StyledTextVenmo
+  ProfileIcon,
+  StyledTextVenmo,
 } from "./ProfileStyles.js";
 import { useState } from "react";
 import LoadingDiv from "../../common/LoadingDiv.js";
@@ -69,17 +69,17 @@ const Profile = () => {
           <BackArrow></BackArrow>
           <StyledText3>Back</StyledText3>
         </ButtonBox>
-      {localStorage.getItem("netid") === user.netid && (
-        <EditProfileButton>
-          <IconButton
-            aria-label="edit"
-            onClick={() => setOpenDialog(true)}
-            variant="outlined"
-          >
-            <EditIcon />
-          </IconButton>
-        </EditProfileButton>
-      )}
+        {localStorage.getItem("netid") === user.netid && (
+          <EditProfileButton>
+            <IconButton
+              aria-label="edit"
+              onClick={() => setOpenDialog(true)}
+              variant="outlined"
+            >
+              <EditIcon />
+            </IconButton>
+          </EditProfileButton>
+        )}
       </TopHeader>
       <ProfileDialog
         openDialog={openDialog}
@@ -87,10 +87,11 @@ const Profile = () => {
         profileUser={user}
       ></ProfileDialog>
       <ProfileCard>
-        <UserPic></UserPic>
+        <ProfileIcon />
         <UserName>{user.firstName + " " + user.lastName}</UserName>
         <College>{user.college}</College>
-        <TextBox onClick={async () => {
+        <TextBox
+          onClick={async () => {
             if (user.phone) {
               navigator.clipboard.writeText(user.phone).then(
                 addToast("Phone Number Copied to Clipboard!", {
@@ -98,9 +99,13 @@ const Profile = () => {
                 })
               );
             }
-          }}>
-            <PhoneIcon/>
-           <StyledText> {user.phone ? user.phone : "Phone Number Unavailable"} </StyledText>
+          }}
+        >
+          <PhoneIcon />
+          <StyledText>
+            {" "}
+            {user.phone ? user.phone : "Phone Number Unavailable"}{" "}
+          </StyledText>
         </TextBox>
         <TextBox
           onClick={() => {
@@ -111,7 +116,7 @@ const Profile = () => {
             );
           }}
         >
-          <MailBox style={{color:"#002140"}}/>
+          <MailBox style={{ color: "#002140" }} />
           <StyledText>{user.netid}@rice.edu</StyledText>
         </TextBox>
         <TextBox
@@ -129,9 +134,7 @@ const Profile = () => {
             }
           }}
         >
-          <StyledText2>
-            Venmo
-          </StyledText2>
+          <StyledText2>Venmo</StyledText2>
           <StyledTextVenmo>
             {user.venmo ? `@${user.venmo}` : "Not Specified"}
           </StyledTextVenmo>
