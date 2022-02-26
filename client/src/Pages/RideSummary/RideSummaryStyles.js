e578e2752fa405fc86793d56a403ec4ddfd2b6b6
Alexis Nicolas <atn4@rice.edu>
2022-02-26 12:58:03 -0600

    Added box shadow to each rider in ride summary
    also changed color of leave ride button

--- client/src/Pages/RideSummary/RideSummaryStyles.js
+++ client/src/Pages/RideSummary/RideSummaryStyles.js
@@ -193,6 +193,7 @@ const OneRiderContainer = styled.div`
   margin: 15px;
   width: 97%;
   font-family: Josefin Sans;
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.25);
 `
 const RiderText = styled.div`
   padding-top: 17px;
@@ -215,7 +216,7 @@ const TextContainer = styled.div`
 `
 const ButtonDiv = styled.button`
   color: #ffffff;
-  background: ${({ disabled, leaveRide }) => leaveRide ? 'red' : !disabled ? '#2075d8' : '#9e9e9e'};
+  background: ${({ disabled, leaveRide }) => leaveRide ? 'rgba(235, 82, 72, 1)' : !disabled ? '#2075d8' : '#9e9e9e'};
   text-align: center;
   font-family: Josefin Sans;
   font-style: normal;
