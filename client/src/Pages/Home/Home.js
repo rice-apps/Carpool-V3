f1649d1a7be79b3a4f6056943ccad164168fee37
Kai Hung <kmh9@rice.edu>
2022-02-27 18:43:16 -0600

    New Landing Page (#155)
    * New Landing Page
    
    * Fixed blank margins

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,24 +1,33 @@
 import React from "react";
 import '../../App.css';
 import { Link } from 'react-router-dom';
-import {WelcomeHeader, CarpoolHeader, Header, SearchButton} from './HomeStyles.js'
+import {FunctionTextTop, 
+        FunctionTextBot, 
+        HomePage, 
+        ImageBox,
+        SearchButton} from './HomeStyles.js'
 
 const Home = () => {
 
     return (
-        <div className="App">
-        <Header>
-            <WelcomeHeader>
-                Welcome to
-            </WelcomeHeader>
-            <CarpoolHeader>
-                Rice Carpool
-            </CarpoolHeader>
-        </Header>
-
-        <Link to="/search"><SearchButton>Start Riding</SearchButton></Link>
-    
+        <div>
+            <HomePage>
+            <ImageBox
+                component="img"
+                alt="Carpool Logo"
+                src="/dark mode.png"
+            >
+            </ImageBox>
+            <FunctionTextTop>
+                Find and coordinate rides with 
+            </FunctionTextTop>
+            <FunctionTextBot>
+                other students - all in one place.
+            </FunctionTextBot>
+            <Link to="/search" style={{ textDecoration:"none" }}><SearchButton>Start Riding</SearchButton></Link>
+        </HomePage>
         </div>
+        
     )
 }
 
--- client/src/Pages/Home/HomeStyles.js
+++ client/src/Pages/Home/HomeStyles.js
@@ -1,42 +1,65 @@
 import styled from 'styled-components'
+import { withStyles } from '@material-ui/core/styles';
+import { Paper, Box } from '@material-ui/core';
 
-const Header = styled.header`
-  display: flex;
-  flex-direction: column;
+const HomePage = withStyles({
+  root:{
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: "center",
+    background: "#002140",
+    backdropFilter: "blur(20px)",
+    height: "100vh", 
+    width: "100vw",
+    overflow: "hidden"
+  }
+})(Paper); 
+
+const ImageBox = withStyles({
+  root:{
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: "center",
+    background: "#002140",
+    height: "50vh", 
+    width: "50vh",
+    maxWidth: "100em",
+    marginTop: "10vh"
+  }
+})(Box); 
+
+const FunctionTextTop = styled.div`
+  position: relative;
   align-items: center;
   justify-content: center;
-  background-color: #002140;
-  width: 100vw;
-  height: 100vh;
-  position:absolute;
-  backdrop-filter: blur(20px);
-`
-const WelcomeHeader = styled.header`
-  align-items: center;
   font-family: Josefin Sans;
   font-style: normal;
   font-weight: normal;
-  font-size: 3.4em;
-  display: flex;
+  font-size: 2.5vh;
   color: white;
-`
-
-const CarpoolHeader = styled.header`
-  align-items: center;
-  justify-content: center;
-  font-family: Josefin Sans;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 5.6em;
-  color: white;
-  margin-top: 3vh;
   line-height:0.90;
   margin-left: 5vw;
   margin-right: 5vw;
+  margin-bottom: 1vh;
+`
+
+const FunctionTextBot = styled.div`
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 2.5vh;
+  color: white;
+  line-height: 0.90;
+  margin-left: 5vw;
+  margin-right: 5vw;
+  margin-bottom: 5vh;
 `
 
 const SearchButton = styled.button`
-  position: absolute;
+  position: relative;
   align-items: center;
   justify-content: center;
   background-color: #FFFFFF;
@@ -44,23 +67,23 @@ const SearchButton = styled.button`
   text-align: center;
   cursor: pointer;
 
-  width:50vw;
+  width: 50vw;
+  max-width: 25em;
   height: 7vh;
   border-radius: 20vw;
 
   font-family: Josefin Sans;
   font-style: normal;
   font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-  color: #002140;
-  top: 70vh;
-  left: 25vw;
+  font-size: 2.5vh;
+  line-height: 3vh;
+  margin-bottom: 10vh;
 `
 
 export {
-  Header,
-  CarpoolHeader, 
-  WelcomeHeader,
+  HomePage, 
+  ImageBox,
+  FunctionTextTop,
+  FunctionTextBot,
   SearchButton,
 }
