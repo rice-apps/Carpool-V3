b7f333853812b9e1e0f641445eb12217a81b01a6
William Yao <47200969+willie-yao@users.noreply.github.com>
2022-04-11 22:41:24 -0500

    Revert "Query whitelist (#189)"
    This reverts commit 8e02ee72227c026e17bebe4e3ac94eea04ed1a22.

--- api/config.js
+++ api/config.js
@@ -6,7 +6,3 @@ export const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 export const SERVICE_URL = process.env.SERVICE_URL;
 export const SECRET = process.env.SECRET;
 export const PORT = process.env.PORT;
-export const REDIS_PORT = process.env.REDIS_PORT;
-export const REDIS_HOST = process.env.REDIS_HOST;
-export const REDIS_USERNAME = process.env.REDIS_USERNAME;
-export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
