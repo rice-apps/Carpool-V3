1c53d48523c9a8e624f290a3f9eafdde40ee22ba
Logan Golia <98609680+logangolia@users.noreply.github.com>
2023-07-24 18:27:10 -0600

    Update DisplayRides.js
    Filter component and filtering logic (latter will be taken out probably)

--- client/src/Pages/Search/DisplayRides.js
+++ client/src/Pages/Search/DisplayRides.js
@@ -1,19 +1,30 @@
+// uber, lyft, and own car. button on front page to click filter and show dropdown. If you choose Uber then only show Uber rides.
+// will need to do export default filter at bottom of filter.js
+
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Grid, IconButton } from '@material-ui/core';
+import { Select, MenuItem, Box, Grid, IconButton, AppBar, Toolbar, Typography } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import Ride from './Ride.js';
 import { GridT, StyledButton } from './DisplayRides.styles'
 import { LoginButton, LoginDialog, LoginDialogActions} from '../Onboarding/Alert.styles.js';
 
+// New import for the Filter component
+//import FilterRide from './FilterRide.js';
+
 // SSO Imports
 import { SERVICE_URL } from '../../config'; 
-const casLoginURL = 'https://idp.rice.edu/idp/profile/cas/login'; 
+const casLoginURL = 'https://idp.rice.edu/idp/profile/cas/login';
+
+
 
 const DisplayRides = (props) => {
 
     const [openAlert, setOpenAlert] = useState(false);
+
+    // New state for the filter
+    const [filter, setFilter] = useState('');
 
     const isEqualRides = (ride1, ride2) => {
         if (ride1._id == null || ride2._id == null) {
@@ -22,6 +33,7 @@ const DisplayRides = (props) => {
 
         return ride1._id === ride2._id;
     }
+
     
     // Determine action when create ride is pressed
     const handleClickCreateRide = () => {
@@ -58,6 +70,34 @@ const DisplayRides = (props) => {
         window.scrollTo(0, 0);
     }
 
+    // New component for the filter
+    // This will render a dropdown with options for Uber, Lyft, and Own Car
+    // The current filter state is used as the value, and when it changes, it updates the filter state
+    const Filter = () => {
+        return (
+            <Select
+                value={filter} // current filter state
+                onChange={(event) => setFilter(event.target.value)} // function to update the filter state when the value changes
+            >
+                <MenuItem value=''>All</MenuItem> // Option for all rides
+                <MenuItem value='Uber'>Uber</MenuItem> // Option for Uber rides
+                <MenuItem value='Lyft'>Lyft</MenuItem> // Option for Lyft rides
+                <MenuItem value='Own Car'>Own Car</MenuItem> // Option for own car rides
+            </Select>
+        );
+    }
+
+    // Define AppBar with Filter
+    const AppBarWithFilter = () => (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    Ride App
+                </Typography>
+                <Filter />
+            </Toolbar>
+        </AppBar>
+    );
 
     const displayRideBottomOfPage = () => {
     
@@ -103,50 +143,63 @@ const DisplayRides = (props) => {
     }
 
     const rideBox = (ridesT) =>  {
-
-         
         if (ridesT === null || ridesT === undefined) {
             return null;
         }
-
+    
         const isValidRidesT = (ridesT!==null && ridesT!==undefined && ridesT.length>0);
-
-        return <GridT container style={{gap: "2vh"}} direction="column" alignItems="center">
-            <Box style={{fontSize: "4vh", paddingTop: '4vh', fontFamily: "Josefin Sans"}}>
-                Matching Rides: 
-            </Box>
-            <Box style={{fontSize: "2.5vh", paddingTop: '.5vh', fontFamily: "Josefin Sans"}}>
-                *All times are in CST
-            </Box>
-            {
-                (isValidRidesT) && ridesT.map((ride, ind) => (<Ride ride={ride} />))
-            }
-            {
-                (!isValidRidesT) && 
-                <Grid item xs={9} justify="center" align='center' alignItems='stretch' style={{height: '100%', display: 'flex', borderRadius: '10px', backgroundColor:"#ddddff"}}>
-                    <Box width={"100%"} height={"100%"} style={{fontSize: "2vh", fontFamily: "Josefin Sans", padding: "1vh"}}>
-                        No rides matched.
-                    </Box>
+    
+        // New: Filter rides based on the selected filter before mapping to Ride components
+        // If no filter is selected (filter === ''), display all rides
+        let filteredRides = ridesT;
+        if (filter) {
+            filteredRides = ridesT.filter(ride => ride.type === filter);
+        }
+    
+        return (
+            <GridT container style={{gap: "2vh"}} direction="column" alignItems="center">
+                <Box style={{fontSize: "4vh", paddingTop: '4vh', fontFamily: "Josefin Sans"}}>
+                    Matching Rides: 
+                </Box>
+                <Box style={{fontSize: "2.5vh", paddingTop: '.5vh', fontFamily: "Josefin Sans"}}>
+                    *All times are in CST
+                </Box>
+                {
+                    (isValidRidesT) && filteredRides.map((ride, ind) => (<Ride ride={ride} />))
+                }
+                {
+                    (!isValidRidesT) && 
+                    <Grid item xs={9} justify="center" align='center' alignItems='stretch' style={{height: '100%', display: 'flex', borderRadius: '10px', backgroundColor:"#ddddff"}}>
+                        <Box width={"100%"} height={"100%"} style={{fontSize: "2vh", fontFamily: "Josefin Sans", padding: "1vh"}}>
+                            No rides matched.
+                        </Box>
+                    </Grid>
+                }
+                <StyledButton style={{fontSize: "1em", color:"#2075D8",  height: '100%', display: 'flex', borderRadius: '10px'}} onClick={() => handleClickCreateRide()}> 
+                        <p> Don't see a match? </p> &nbsp; Create a new ride 
+                </StyledButton>
+                {
+                    <div style = {{paddingTop: '1vh', fontSize: "4vh", fontFamily: "Josefin Sans"}}>All Open Rides:</div>
+                }
+                {
+                    props.ridesPossible.filter((ride) => { 
+                        return !ridesT.some(e => isEqualRides(ride, e))}).filter(ride => (ride.spots - ride.riders.length > 0)).map((ride, ind) => (<Ride ride={ride} />))
+                }
+                <Grid item justify="center" align='center' style={{ display: 'flex', alignItems: 'center', fontFamily: "Josefin Sans", fontSize: "2vh", color: "#012E62"}}>
+                    no more results
                 </Grid>
-            }
-            <StyledButton style={{fontSize: "1em", color:"#2075D8",  height: '100%', display: 'flex', borderRadius: '10px'}} onClick={() => handleClickCreateRide()}> 
-                    <p> Don't see a match? </p> &nbsp; Create a new ride 
-            </StyledButton>
-            {
-                <div style = {{paddingTop: '1vh', fontSize: "4vh", fontFamily: "Josefin Sans"}}>All Open Rides:</div>
-            }
-            {
-                props.ridesPossible.filter((ride) => { 
-                    return !ridesT.some(e => isEqualRides(ride, e))}).filter(ride => (ride.spots - ride.riders.length > 0)).map((ride, ind) => (<Ride ride={ride} />))
-            }
-            <Grid item justify="center" align='center' style={{ display: 'flex', alignItems: 'center', fontFamily: "Josefin Sans", fontSize: "2vh", color: "#012E62"}}>
-                no more results
-            </Grid>
-        </GridT>;
-      }
+            </GridT>
+        );
+    }
+    
 
+          // The component returns a div containing the filter and the rides
+    // It first renders the Filter component, then it calls the rideBox function to render the rides
+    // Finally, it calls the displayRideBottomOfPage function to render the bottom of the page
     return (
         <div>
+            {/* Add the Filter component to the return statement */}
+            <AppBarWithFilter />
             <div>
                 {
                     rideBox(props.ridesT)
@@ -160,5 +213,6 @@ const DisplayRides = (props) => {
         </div>
     )
 }
+
 
 export default DisplayRides
