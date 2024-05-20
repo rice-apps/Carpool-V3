e6b6aaca7898fed3fd5e2d5462e3c4319d0ac625
Ahmed A <95593064+AhmedA1559@users.noreply.github.com>
2024-05-19 19:40:39 -0500

    Revert "Debug"

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,9 +1,11 @@
 // Apollo Client Setup
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split, gql } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { setContext } from '@apollo/link-context';
 
 // Apollo Subscriptions Setup
-import { GRAPHQL_URL, SERVICE_URL } from './config';
+import { WebSocketLink } from '@apollo/link-ws';
+import { GRAPHQL_URL, GRAPHQL_WS_URL, SERVICE_URL } from './config';
 
 // Wraps our requests with a token if one exists
 // Copied from: https://www.apollographql.com/docs/react/v3.0-beta/networking/authentication/
@@ -24,22 +26,40 @@ const httpLink = new HttpLink({
     uri: GRAPHQL_URL
 });
 
+// WebSocket Backend Link
+const wsLink = new WebSocketLink({
+    uri: GRAPHQL_WS_URL,
+    options: {
+        reconnect: true
+    }
+});
+
+// Uses wsLink for subscriptions, httpLink for queries & mutations (everything else)
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
+);
+
 // Setup cache
 const cache = new InMemoryCache();
 
 // Initialize Client
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(httpLink),
+    link: authLink.concat(splitLink),
 });
 
 // Initial local state
 const initialState = {
     service: SERVICE_URL,
-    user: {
-        recentUpdate: false,
-        _id: ''
-    }
+    recentUpdate: false
 }
 
 // Initialize cache with a state
@@ -47,10 +67,7 @@ cache.writeQuery({
     query: gql`
         query InitialState {
             service
-            user {
-                recentUpdate
-                _id
-            }
+            recentUpdate
         }
   `,
     data: initialState
